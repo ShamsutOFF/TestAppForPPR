3105ff469acbeea158f9294a0ebfe6083df8db45
ShamsutOFF <shamsutoff@yandex.ru>
2022-02-09 13:09:18 +0300

    Проект готов

--- app/src/main/java/com/example/testappforppr/MainActivity.kt
+++ app/src/main/java/com/example/testappforppr/MainActivity.kt
@@ -5,12 +5,18 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.testappforppr.view.FibonacciNumbersScreen
 import com.example.testappforppr.view.PrimeNumbersScreen
 import com.google.accompanist.pager.*
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 private const val TAG = "@@@ MainActivity"
@@ -37,14 +43,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HorizontalPager(mainViewModel: MainViewModel) {
     val pagerState = rememberPagerState(0)
-    HorizontalPager(2, state = pagerState) { page ->
-        when (page) {
-            0 -> {
-                PrimeNumbersScreen(mainViewModel.primeNumbers)
+    val scope = rememberCoroutineScope()
+    Column() {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions), height = 5.dp, color = Color.Red
+                )
             }
-            1 -> {
-                FibonacciNumbersScreen(mainViewModel.fibonacciNumbers)
+        ) {
+            val pages = listOf("Проятые числа", "Числа Фибоначи")
+            pages.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text(title) },
+                    selected = pagerState.currentPage == index,
+                    onClick = { scope.launch { pagerState.scrollToPage(index) }},
+                )
+            }
+        }
+
+        HorizontalPager(2, state = pagerState) { page ->
+            when (page) {
+                0 -> {
+                    PrimeNumbersScreen(mainViewModel.primeNumbers)
+                }
+                1 -> {
+                    FibonacciNumbersScreen(mainViewModel.fibonacciNumbers)
+                }
             }
         }
     }
+
+
 }
